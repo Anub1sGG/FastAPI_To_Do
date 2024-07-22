8d473c5bf61a26f6b7c2431d0fd76b4c08613716
Anub1sGG <yyychekhanyuk@gmail.com>
2024-07-22 14:35:48 +0300

    fix: apdate requirements.txt

--- repository.py
+++ repository.py
@@ -1,27 +1,44 @@
-from sqlalchemy import select
-
-from database import new_session, TaskOrm
-from schemas import STaskAdd, STask
-
-
-class TaskRepository:
-    @classmethod
-    async def add_one(cls, data: STaskAdd) -> int:
-        async with new_session() as session:
-            task_dict = data.model_dump()
-
-            task = TaskOrm(**task_dict)
-            session.add(task)
-            await session.flush()
-            await session.commit()
-            return task.id
-
-    @classmethod
-    async def find_all(cls) -> list[STask]:
-        async with new_session() as session:
-            query = select(TaskOrm)
-            result = await session.execute(query)
-            task_models = result.scalars().all()
-            task_schemas = [STask.model_validate(task_model) for task_model in task_models]
-            return task_schemas
-
+aiosqlite==0.20.0
+annotated-types==0.7.0
+anyio==4.4.0
+certifi==2024.6.2
+click==8.1.7
+dnspython==2.6.1
+email_validator==2.2.0
+exceptiongroup==1.2.1
+fastapi==0.111.0
+fastapi-cli==0.0.4
+greenlet==3.0.3
+h11==0.14.0
+httpcore==1.0.5
+httptools==0.6.1
+httpx==0.27.0
+idna==3.7
+iniconfig==2.0.0
+Jinja2==3.1.4
+markdown-it-py==3.0.0
+MarkupSafe==2.1.5
+mdurl==0.1.2
+orjson==3.10.6
+packaging==24.1
+pluggy==1.5.0
+pydantic==2.8.0
+pydantic_core==2.20.0
+Pygments==2.18.0
+pytest==8.2.2
+python-dotenv==1.0.1
+python-multipart==0.0.9
+PyYAML==6.0.1
+rich==13.7.1
+shellingham==1.5.4
+sniffio==1.3.1
+SQLAlchemy==2.0.31
+starlette==0.37.2
+tomli==2.0.1
+typer==0.12.3
+typing_extensions==4.12.2
+ujson==5.10.0
+uvicorn==0.30.1
+uvloop==0.19.0
+watchfiles==0.22.0
+websockets==12.0
